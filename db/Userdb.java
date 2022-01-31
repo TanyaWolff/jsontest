fcbe67ec44414e9fae10c1e8addbe553554567ef
twolff <tanwolff@gmail.com>
2022-01-31 11:30:15 -0500

    Read users from database

--- db/Userdb.java
+++ db/Userdb.java
@@ -6,11 +6,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 class Userdb {
 
@@ -25,21 +27,47 @@ class Userdb {
 	public Connection getConn() {
 		return conn;
 	}
-
+	/* Create a new user and add it to the database via the Userdb object (this class).
+	   Then get all users from the database and display them.
+	   Note that roles column is not in the database, so setting a user role and hoping to see
+	   it appear when reading the database will leave you disenchanted.
+	*/
 	public static void main(String[] args) throws SQLException {
 		System.out.println("Connect to db");
 		Userdb udb=new Userdb();
 		User u = new User("Test1", "User1","test1user1@example.com");
+		List<String> newroles = u.getRoles();
+		newroles.add("dbuser");
+		u.setRoles(newroles);
+		System.out.println("Adding user "+u.print());
+		// put user in database
 		udb.insert_user(u);
+		// read users from database.
+		// All users will have the 'user' role which was set in the contructor.
+		udb.getUsersFromDb();
 		DbUtils.close(udb.getConn());
+
 	}
 	public void insert_user(User u) throws SQLException {
 		// insert something
+		System.out.println("Inserting user into db: "+u.print());
 		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 		int count = queryrunner.update(conn,
-			"INSERT INTO users(id,firstname,lastname,email,created_on) VALUES (?,?,?,?,?)",
-			14,u.getFirstname(),u.getLastname(),u.getEmail(),timestamp);
+			"INSERT INTO users(firstname,lastname,email,created_on) VALUES (?,?,?,?)",
+			u.getFirstname(),u.getLastname(),u.getEmail(),timestamp);
 		System.out.println(count + " record(s) inserted");
+	}
+	public void getUsersFromDb() throws SQLException {
+		try {
+			ResultSetHandler<List<User>> resultHandler = new BeanListHandler<User>(User.class);
+			List<User> users = queryrunner.query(conn, "SELECT * FROM users", resultHandler);
+			for(User u: users ) {
+				System.out.println(u.print());
+				System.out.println("***");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private Connection conn;
--- pkg/User.java
+++ pkg/User.java
@@ -7,25 +7,62 @@ public class User {
   public User() {
     first_name="John";
     last_name="Smith";
-    roles=new ArrayList<>();
+    this.roles=new ArrayList<>();
+    this.roles.add("user");
   }
   public User(String first, String last) {
     first_name=first;
     last_name=last;
+    this.roles=new ArrayList<>();
   }
   public User(String first, String last, String email) {
     this.first_name=first;
     this.last_name=last;
+    this.roles=new ArrayList<>();
     this.email=email;
+  }
+  public int getId() {
+	return id;
+  }
+  public void setId(int id) {
+	this.id = id;
   }
   public String getFirstname() {
 	return first_name;
   }
+  public void setFirstname(String fn) {
+	this.first_name = fn;
+  }
   public String getLastname() {
 	return last_name;
   }
+  public void setLastname(String ln) {
+	this.last_name = ln;
+  }
   public String getEmail() {
 	return email;
+  }
+  public void setEmail(String em) {
+	this.email=em;
+  }
+  public List<String> getRoles() {
+	return roles;
+  }
+  public void setRoles(List<String> rls) {
+	this.roles=rls;
+  }
+  public String print() {
+    String rolesStr = (roles == null || roles.isEmpty()) ? "no roles" : String.join(",", roles);
+    String userStr =
+    "id: "+id+
+    "\nuserid: "+userid+
+    "\nfirst_name: "+first_name+
+    "\nlast_name: "+last_name+
+    "\nroles: "+rolesStr+
+    "\nemail: "+email+
+    "\nphone: "+phone+
+    "\naddress: "+address;
+    return userStr;
   }
   String first_name;
   String last_name;
@@ -33,6 +70,6 @@ public class User {
   String email;
   String phone;
   String userid;
-  String id;
+  int id;
   List<String> roles;
 }
